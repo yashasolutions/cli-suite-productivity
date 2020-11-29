60eef2f86cb00c37029e39dae21038a27af8e48b
Yasha <738693-yashasolutions@users.noreply.gitlab.com>
2020-11-29 14:31:13 +0100

    "fixed scope issues"

--- cmd/gshare.sh
+++ cmd/gshare.sh
@@ -9,4 +9,6 @@ FILE=$1
 #echo -e "Uploading testimage.jpg \nUploaded 1d2m1e8PRC3OYeDyeup0okDYx6OusKSRo at 48.3 KB/s, total 65.4 KB" | sed -n '2p' | cut -d" " -f2 | xargs glink
 
 
-gdrive upload $FILE | sed -n '2p' | cut -d" " -f2 | xargs glink
+FILEID=$(gdrive upload $FILE | sed -n '2p' | cut -d" " -f2)
+gdrive share $FILEID 
+glink $FILEID
