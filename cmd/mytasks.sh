e4d3ae5489598e8abcb98e7a166fdaa278c72cc5
Yasha <738693-yashasolutions@users.noreply.gitlab.com>
2021-01-16 16:11:26 +0100

    fine tuning personal task system

--- cmd/mytasks.sh
+++ cmd/mytasks.sh
@@ -3,18 +3,24 @@
 
 REPO="my/repo"
 . $HOME/.gtaskrc
+cd $LOCALCLONE #defined in gtaskrc
+
 
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -l|list) 
+        l|list) 
 					glab issue list -R $REPO; exit ;;
-        -v|view) glab issue view $2 -R $REPO; exit ;;
-        -c|close) glab issue close $2 -R $REPO; exit ;;
-        -w|web) glab issue view -w $2 -R $REPO; exit;;
-        -a|add) glab issue create -t $2 -d "" --no-editor -R $REPO; exit; shift;;
+        v|view) glab issue view $2 -R $REPO; exit ;;
+        c|close) glab issue close $2 -R $REPO; exit ;;
+        w|web) glab issue view -w $2 -R $REPO; exit;;
+        a|add) glab issue create -t $2 -d "" --no-editor -R $REPO; exit; shift;;
+        i|interactive) glab issue create ; exit; shift;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
 
+# if no argument was given
+# start interactive mode
+glab issue create 
